1c38635ad5a0b3ef5856329722a8341aee602ae3
Hristo Mitsev <dakillaroshun@gmail.com>
2020-01-18 20:55:14 +0200

    Add error bubbles under the register form
    Red error bubbles have been added under register form

--- js/index.js
+++ js/index.js
@@ -1,11 +1,15 @@
 $(document).ready(() => {
 
+    $(".errorMessages").hide();
+
     $("#navigationPageHome").click(showIndexPage);
     $("#navigationPageSignIn").click(showSignInPage);
     $("#navigationPageRegister").click(showRegisterPage);
     $("#navigationPageAbout").click(showAboutPage);
-    $("#indexPageSignInButton").click(signIn);
-    $("#indexPageRegisterButton").click(register);
+    $("#indexPageSignInButton").click(showSignInPage);
+    $("#indexPageRegisterButton").click(showRegisterPage);
+    $("#signInPageButton").click(signIn);
+    $("#registerPageButton").click(register);
 
     if (doesContainCookies()) {
         showSignInPage();
@@ -38,6 +42,8 @@ $(document).ready(() => {
     }
 
     function showIndexPage() {
+        $(document).prop("title", "My Library");
+
         hideAllPages();
         $("#indexPage").show();
 
@@ -46,7 +52,8 @@ $(document).ready(() => {
     }
 
     function showSignInPage() {
-        console.log("asd");
+        $(document).prop("title", "My Library - Sign In");
+
         hideAllPages();
         $("#signInPage").show();
 
@@ -55,6 +62,8 @@ $(document).ready(() => {
     }
 
     function showRegisterPage() {
+        $(document).prop("title", "My Library - Register");
+
         hideAllPages();
         $("#registerPage").show();
 
@@ -66,12 +75,87 @@ $(document).ready(() => {
         hideAllPages();
         $("#aboutPage").show();
 
+        $(document).prop("title", "My Library - About");
+
         showAllNavigationPages();
         $("#navigationPageAbout").hide();
     }
 
     function register() {
         showRegisterPage();
+
+        let firstName = $("#registerPageFirstName").val();
+        let lastName = $("#registerPageLastName").val();
+        let username = $("#registerPageUsername").val();
+        let email = $("#registerPageEmail").val();
+        let password = $("#registerPagePassword").val();
+        let repeatPassword = $("#registerPageRepeatPassword").val();
+
+        if(!validateFirstName(firstName)){
+            showRegisterErrorMessage("Invalid first name!");
+            return;
+        }
+        if(!validateLastName(lastName)){
+            showRegisterErrorMessage("Invalid last name!");
+            return;
+        }
+        if(!validateUsername(username)){
+            showRegisterErrorMessage("Invalid username!");
+            return;
+        }
+        if(!validateEmail(email)){
+            showRegisterErrorMessage("Invalid email!");
+            return;
+        }
+        if(!validatePassword(password)){
+            showRegisterErrorMessage("Invalid password! Your password must contain at least one uppercase letter, one lowercase letter and at least one special character");
+            return;
+        }
+        if(repeatPassword !== password){
+            showRegisterErrorMessage("The two passwords don't match!");
+            return;
+        }
+    }
+
+    function validateFirstName(firstName){
+        if(firstName.length > 25){
+            return false;
+        }
+
+        let re = /^[a-zA-Z]+(([\'\,\.\-][a-zA-Z])?[a-zA-Z]*)*$/;
+        return re.test(firstName);
+    }
+
+    function validateLastName(lastName){
+        if(lastName.length > 25){
+            return false;
+        }
+
+        let re = /^[a-zA-Z]+(([\'\,\.\-][a-zA-Z])?[a-zA-Z]*)*$/;
+        return re.test(lastName);
+    }
+
+    function validateUsername(username) {
+        let re = /^[a-z0-9_-]{3,16}$/;
+        return re.test(username.toLowerCase());
+    }
+
+    function validateEmail(email) {
+        let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email.toLowerCase());
+    }
+
+    function validatePassword(password) {
+        let re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return re.test(password);
+    }
+
+    function showRegisterErrorMessage(message){
+        $("#registerPageErrorMessage").show().text(message);
+
+        setTimeout(()=>{
+            $("#registerPageErrorMessage").fadeOut("fast");
+        }, 2000);
     }
 
     function signIn() {
